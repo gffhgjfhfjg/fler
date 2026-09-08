04e313d2ef929bbfa4228aa3d98bd9df42171564
gffhgjfhfjg <gffhgjfhfjg@users.noreply.github.com>
2026-09-08 00:21:38 +0000

    chore: bump version to 1.9.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.ai.fler"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.9.4"
+        versionCode = 15
+        versionName = "1.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
